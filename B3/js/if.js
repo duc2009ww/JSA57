6afee75b8121bd32edda850631cbc5cd1acdc8ab
duc2009ww <hg30950376802@gmail.com>
2025-08-13 09:00:03 -0500

    B3

---
+++ B3/js/if.js
@@ -0,0 +1,20 @@
+console.group("Lenh dieu kien if - else");
+const arr = [];
+const obj = {};
+const obj1 = { name: "A", age: 18 };
+
+//{}: chứa nhièu hơn 1 câu lệnh 
+// arr rỗng thì trả về true -> không thể kiểm tra if (arr) -> arr.length
+if (arr.length) {
+    console.log("arr");
+}console.log("khong co gia tri");
+
+// toan tu ?. + HTML DOM -----------------------
+const tag = document.querySelector("#tag");
+if (tag) {
+  tag.addEventListener("click", () => console.log("clicked"));
+}
+
+tag?.addEventListener("click", () => console.log("clicked"));
+
+console.groupEnd("Lenh dieu kien if - else");
---
+++ B3/js/sosanh.js
@@ -0,0 +1,21 @@
+console.group("Toan tu So sanh");
+let a = 100;
+let b = "100";
+let c = 12.3;
+
+console.log(m < o);
+console.log(m > o);
+console.log(m >= o);
+console.log(m <= o);
+// ==: so sanh gia tri
+console.log(m == n); // true
+// ===: SO SANH NGHIEM NGAT -> so sanh gia tri + kieu du lieu
+console.log(m === n); // false
+
+// !=: so sanh gia tri khac nhau
+console.log(m != n); // false
+
+// !==: so sanh gia tri + kieu du lieu khac nhau
+console.log(m !== n); // true
+
+console.groupEnd("Toan tu So sanh");
