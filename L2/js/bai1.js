95132a9ecd76f6de74e1664ccdf04c24dae68603
duc2009ww <hg30950376802@gmail.com>
2025-09-03 09:02:38 -0500

    B6

---
+++ L2/js/bai1.js
@@ -0,0 +1,6 @@
+//khai baos bieens age banf tu khoa let voi gia tri la so 25
+
+// var temperature = 30;
+// var greeting = 
+// var isjavaScriptFun = true;
+// var 
---
+++ L4/js/script.js
@@ -0,0 +1,12 @@
+const students = [
+    { id: 1, name: "Alice johnson" },
+    { id: 2, name: "Alice johnson" },
+    { id: 3, name: "Alice johnson" },
+    { id: 4, name: "Alice johnson" },
+    { id: 5, name: "Alice johnson" },
+    { id: 6, name: "Alice johnson" },
+    { id: 7, name: "Alice johnson" },
+    { id: 8, name: "Alice johnson" },
+    { id: 9, name: "Alice johnson" },
+    { id: 10, name: "Alice johnson" },
+];
